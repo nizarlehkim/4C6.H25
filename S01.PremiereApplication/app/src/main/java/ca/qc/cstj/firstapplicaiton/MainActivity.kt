0fd52d0033b140533b2298ca6ebb0101124e6f52
2183619 <2183619@cstj.qc.ca>
2025-01-22 11:25:42 -0500

    chore: project created

--- S01.PremiereApplication/app/src/main/java/ca/qc/cstj/firstapplicaiton/MainActivity.kt
+++ S01.PremiereApplication/app/src/main/java/ca/qc/cstj/firstapplicaiton/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ca.qc.cstj.firstapplicaiton.ui.screens.GameScreen
 import ca.qc.cstj.firstapplicaiton.ui.theme.FirstApplicaitonTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +28,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FirstApplicaitonTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Nizar",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+                GameScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirstApplicaitonTheme {
-        Greeting("Nizar")
-    }
-}
